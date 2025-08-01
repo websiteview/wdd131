9721feff05c3c2ffbc279e747e0bf7b92c2fc5bf
byui-professional <men22027@byui.edu>
2025-07-31 20:31:01 -0600

    final form

--- scripts/products.js
+++ scripts/products.js
@@ -1,9 +1,10 @@
+// products.js
 const products = [
-  { id: 1, name: "Phone" },
-  { id: 2, name: "Laptop" },
-  { id: 3, name: "Tablet" },
-  { id: 4, name: "Monitor" },
-  { id: 5, name: "Keyboard" }
+  { id: "fc-1888", name: "Flux Capacitor" },
+  { id: "fc-2050", name: "Power Laces" },
+  { id: "fs-1987", name: "Time Circuits" },
+  { id: "ac-2000", name: "Low Voltage Reactor" },
+  { id: "jj-1969", name: "Warp Equalizer" }
 ];
 
 export default products;
